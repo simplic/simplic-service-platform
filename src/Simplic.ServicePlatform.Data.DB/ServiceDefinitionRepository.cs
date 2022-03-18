4a26b3a4d6363d3fd8d7d592c38c4301e4b62f11
4eck-qed <ssindustries@web.de>
2022-03-18 14:49:54 +0100

    added indentation for writing jsons

--- src/Simplic.ServicePlatform.Data.DB/ServiceDefinitionRepository.cs
+++ src/Simplic.ServicePlatform.Data.DB/ServiceDefinitionRepository.cs
@@ -55,7 +55,7 @@ namespace Simplic.ServicePlatform.Data.DB
         /// <inheritdoc/>
         public Task Save(ServiceDefinition service)
         {
-            var json = JsonConvert.SerializeObject(service);
+            var json = JsonConvert.SerializeObject(service, Formatting.Indented);
 
             fileService.WriteAllText($"/services/{service.ServiceName}.json", json);
             return Task.CompletedTask;
