9a3e320ef1693c35916762f7beafe5b31bd58e9f
Segieth <Segieth@TSDEV01>
2022-03-29 09:15:06 +0200

    Temp Service View, updatet service name text box.

--- src/Simplic.ServicePlatform.UI/Properties/AssemblyInfo.cs
+++ src/Simplic.ServicePlatform.UI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.43000.323")]
-[assembly: AssemblyFileVersion("1.0.43000.323")]
+[assembly: AssemblyVersion("1.0.43500.323")]
+[assembly: AssemblyFileVersion("1.0.43500.323")]
