a5283313e9d31b21e68566fa9119a8a1d050005b
Segieth <Segieth@TSDEV01>
2022-03-30 09:37:33 +0200

    Service plattform ui final.

---
+++ src/Simplic.ServicePlatform.UI/ServicePlattformApplicationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Simplic.ServicePlatform.UI
+{
+    public static class ServicePlattformApplicationHelper
+    {
+
+        public static void ShowEditor(Guid _)
+        {
+            IServiceClient serviceClient = CommonServiceLocator.ServiceLocator.Current.GetInstance<IServiceClient>();
+            Application.Current.Dispatcher.Invoke(async () =>
+            {
+                var service = await serviceClient.GetAllServices();
+                var view = new ServiceView(serviceClient);
+                view.Show();
+            });
+
+
+        }
+    }
+}
