492b8099ff6e5e1910d19055f207ce508b9de485
Segieth <Segieth@TSDEV01>
2022-03-30 09:39:30 +0200

    Service plattform ui final.

--- src/Simplic.ServicePlatform.UI/ServicePlattformApplicationHelper.cs
+++ src/Simplic.ServicePlatform.UI/ServicePlattformApplicationHelper.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace Simplic.ServicePlatform.UI
 {
+    /// <summary>
+    /// Static class with method to open service view ui in simplic studio.
+    /// </summary>
     public static class ServicePlattformApplicationHelper
     {
-
+        /// <summary>
+        /// This method gets called in simplic studio, it opens the service application configuration ui.
+        /// </summary>
+        /// <param name="_"></param>
         public static void ShowEditor(Guid _)
         {
             IServiceClient serviceClient = CommonServiceLocator.ServiceLocator.Current.GetInstance<IServiceClient>();
